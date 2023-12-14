a2692567dd5dae3d2c7ce8e38860ffa186a5d244
esdras <135667004+veigv@users.noreply.github.com>
2023-12-13 21:01:35 -0300

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Apple',
+        productNumber: 'AAPL34',
+        paymentStatus: 'R$  3.278,30',
+        status: 'Concluído',
+    },
+    {
+        productName: 'Tesla Inc',
+        productNumber: 'TSLA34',
+        paymentStatus: 'R$  5.849,34',
+        status: 'Concluído',
+    },
+    {
+        productName: 'Spotify Tech',
+        productNumber: 'S1PO34',
+        paymentStatus: 'R$  2.455,20',
+        status: 'Concluído',
+    },
+]
